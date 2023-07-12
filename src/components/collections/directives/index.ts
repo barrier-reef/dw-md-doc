4fb6074c5b79107fd6290216ea3496c00f6e38d6
Yuanhao Geng <1801214626@qq.com>
2023-07-11 21:41:35 -0400

    implement html-board

--- src/components/collections/directives/index.ts
+++ src/components/collections/directives/index.ts
@@ -1,7 +1,9 @@
 import { multipleChoiceMapper } from "./multiple-choice/index.js";
 import { admonitionMapper } from "./admonitions/index.js";
+import HTMLBoard from "./HTMLBoard.vue";
 
 export const directiveMapper: Record<string, any> = {
   ...multipleChoiceMapper,
   ...admonitionMapper,
+  "html-editable-board": HTMLBoard,
 };

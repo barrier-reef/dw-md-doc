5ac0809e6b777ec466eb365fcd619220f2017bba
Yuanhao Geng <1801214626@qq.com>
2023-07-17 19:42:36 -0400

    support error Ansi render

--- src/components/collections/exec/Exec.vue
+++ src/components/collections/exec/Exec.vue
@@ -1,5 +1,14 @@
 <template>
-  <textarea v-model="inputCode" rows="20" cols="50"></textarea>
+  <textarea
+    v-model="inputCode"
+    class="code-textarea"
+    :class="{ 'is-error': jpError }"
+    id="myTextarea"
+    @input="autoResize"
+  ></textarea>
+  <div v-if="jpError">
+    <pre class="error-result"><Ansi>{{ensureString(jpError)}}</Ansi></pre>
+  </div>
   <div v-if="jpStream">
     <pre>{{ jpStream }}</pre>
   </div>
@@ -14,6 +23,7 @@
 <script setup lang="ts">
 import axios from "axios";
 import { ref } from "vue";
+import Ansi from "ansi-to-vue3";
 
 const props = defineProps<{
   node: any;
@@ -25,6 +35,27 @@ const jpHtml = ref<any>(null);
 const jpImage = ref<any>(null);
 const jpError = ref<any>(null);
 const jpStream = ref<any>(null);
+
+function ensureString(
+  maybeString: string[] | string | undefined,
+  joinWith = "\n"
+): string {
+  if (!maybeString) return "";
+  if (typeof maybeString === "string") return maybeString;
+  if (maybeString.join) return maybeString.join(joinWith);
+  return maybeString as unknown as string;
+}
+
+function autoResize() {
+  const textarea = document.getElementById("myTextarea")!;
+  textarea.style.height = "auto"; // 重置高度为自适应
+
+  // 计算内容的滚动高度
+  const scrollHeight = textarea.scrollHeight;
+
+  // 设置textarea的高度为内容的滚动高度
+  textarea.style.height = scrollHeight + "px";
+}
 
 async function execCode() {
   try {
@@ -48,3 +79,30 @@ async function execCode() {
   }
 }
 </script>
+
+<style scoped>
+.code-textarea {
+  width: 100%;
+  height: auto;
+  resize: none;
+  background-color: #f7f7fc;
+  margin: 0;
+  padding: 14px;
+  box-sizing: border-box;
+  border: 2px solid #dbdfe3;
+  border-radius: 4px;
+  overflow-y: hidden;
+}
+.code-textarea:focus {
+  outline: none;
+  border: 2px solid #479ce7;
+  border-radius: 4px;
+}
+.is-error {
+  border: 2px solid rgb(202, 115, 115);
+}
+.error-result {
+  margin: 0;
+  padding-left: 10px;
+}
+</style>
